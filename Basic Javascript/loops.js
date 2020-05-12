7812be34befd736ff51d71ed3f702cc7056c5112
yotam <ynagor@gmail.com>
2020-05-12 20:52:13 +0300

    more answers

--- Basic Javascript/loops.js
+++ Basic Javascript/loops.js
@@ -98,4 +98,68 @@ console.log(num);
 
 // כתוב תוכנית שקולטת מהמשתמש מספרים ומכניסה אותם למערך 
 // עד שהמשתמש מכניס -1 
-// לאחר מכן התוכנית תקלוט שתי מספרים נוספים 
+// לאחר מכן התוכנית תקלוט שתי מספרים נוספים ותדפיס את כל האיברים במערך ששוים לסכום שתי המספרים האחרונים שנקלטו
+
+const num = [];
+let x = parseInt(prompt("Enter a Number"));
+
+while(x !== -1) {
+    num.push(x);
+    x = parseInt(prompt("Enter a Number"));
+}
+
+let y = parseInt(prompt("Enter first number"));
+let z = parseInt(prompt("Enter second number"));
+
+for (let i = 0; i < num.length; i++) {
+    if(num[i] === y + z) {
+        console.log(num[i])
+        
+    }
+}
+
+
+
+
+// כתוב תוכנית שקולטת מספרים למערך עד שהמשתמש מכניס -1
+// לאחר מכן היא קולטת שתי מספרים נוספים
+// והיא בודקת האם קיים זוג מספרים עוקב במערך ששווה לשתי המספרים שנקלטו בהתאמה
+// ברגע שנמצאו זוג מספרים התוכנית תדפיס טרו ותפסיק לרוץ
+// אחרת התוכנית תדפיס פולס
+
+const num = [];
+let x = parseInt(prompt("enter number"));
+
+while(x !== -1) {
+    num.push(x);
+    x = parseInt(prompt("Enter a number"));
+}
+
+let y = parseInt(prompt("Enter first number"));
+let z = parseInt(prompt('enter second number'));
+let wasFound = false
+
+for (let i = 0; (!wasFound && (i < num.length - 1)); i++) {
+    if(y === num[i] && z === num[i + 1]) {
+        console.log(true);
+        wasFound = true;
+    } 
+
+}
+
+// כתוב תוכנית שמדפיסה את לוח הכפל
+
+const numCounter = [1,2,3,4,5,6,7,8,9,10];
+for (i = 0; i < numCounter.length; i++) {
+    for(a = 0; a < numCounter.length; a++) {
+        console.log(numCounter[i] * numCounter [a])
+    }
+}
+
+
+
+
+
+
+// כתוב תוכנית שקולטת מספרים למערך עד שהמשתמש מכניס -1
+// 
