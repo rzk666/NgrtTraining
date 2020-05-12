79af63b094860304604ec947ae644c2a19973844
yotam <ynagor@gmail.com>
2020-05-12 17:47:58 +0300

    answers

--- Basic Javascript/Comparison/Notes.js
+++ Basic Javascript/Comparison/Notes.js
@@ -1,9 +1,12 @@
 // What will the following print?
+// *** False * False = TRUE
+// *** True 8 False = FALSE
+// *** undefined/ 0 / `   ` / 
 
-console.log(!undefined)
-console.log(!0)
-console.log(!'')
-console.log(!'Test')
-console.log(!53)
-console.log(!true)
-console.log(!false)
+console.log(!undefined) === true
+console.log(!0) === true
+console.log(!'') === true
+console.log(!'Test') === false
+console.log(!53) === false
+console.log(!true) === false
+console.log(!false) === true
