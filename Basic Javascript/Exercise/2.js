0af82694041744839421d3fd6e551ed7cc68b037
Razi <razi@journeyo.com>
2020-05-15 19:30:53 +0300

    [EXEC] Javascript 2

---
+++ Basic Javascript/Exercise/2.js
@@ -0,0 +1,19 @@
+// TARGILIM #2 //
+
+// כתוב פונקציה שמקבלת אובייקט משתמשים ומספר שנים, הפונקציה תחזיר אובייקט עם גילאי האנשים לאחר מספר השנים
+// דוגמא
+// usersObject = { david: 15, razi:27, ngrt:28 } // זה המבנה של אובייקט משתמשים
+// myFunc(usersObject) should return { david: 20, razi:32, ngrt:33}
+
+// כתוב פונקציה שמקבלת מחרוזת. הפונקציה תצמצם כל רצף תווים עוקבים לתו אחד:
+// דוגמא
+// string = "hhhhelllllooooo"
+// myFunc(string) should return "helo"
+
+// כתוב מחשבון. המחשבון יעבוד בצורה הבאה: המשתמש יזין 2 מספרים ואת אחת מהפעולות הבאות כתו בודד:
+// * / % + -
+// המחשבון יעבד את הקלט בהתאם וידפיס את התוצאה
+// הקלט יפסק כשהמשתמש יזין 0 0
+// Note -> Learn and use switch case
+// Note -> Make sure dividing by zero is handled properly
+
