c9c2a1109fe41684f1170acf4fbd9b4a7131c25d
Razi <razi@journeyo.com>
2020-07-09 16:59:42 +0300

    [FIRST] First allah2

--- Basic React/my_first_react_app/src/App.jsx
+++ Basic React/my_first_react_app/src/App.jsx
@@ -3,19 +3,8 @@ import React from 'react';
 import styles from './App.module.scss';
 
 const App = () => {
-  const x = 5;
   return (
-    <div
-      className={styles.main_container}
-    >
-      <div className={styles.btn}>
-        5
-      </div>
-      <div className={styles.container}>
-        <div className={styles.btn}>
-          10
-        </div>
-      </div>
+    <div>
     </div>
   );
 };
