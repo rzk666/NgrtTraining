fb577d36500e8e3d3523adf2630423a5b1882f76
Razi <razi@journeyo.com>
2020-07-09 16:25:40 +0300

    [WIP] BLA

---
+++ Basic React/my_first_react_app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => {
+  const x = 5;
+  return (
+    <div
+      className={x}
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+      }}
+    >
+      {x}
+    </div>
+  );
+};
+
+export default App;
