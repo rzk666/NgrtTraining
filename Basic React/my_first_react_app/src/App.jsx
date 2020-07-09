efaea0b43f6b4c119e7340119987ea68f424a863
yotam <ynagor@gmail.com>
2020-07-09 17:58:06 +0300

    [WIP] REACT BITCH

--- Basic React/my_first_react_app/src/App.jsx
+++ Basic React/my_first_react_app/src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 // styles
 import styles from './App.module.scss';
 
-const App = () => {
-  return (
-    <div>
-    </div>
-  );
-};
+const App = () => (
+  <div className={styles.site_container}>
+    <div className={styles.blue_square}>ZLADIM</div>
+    <div className={styles.green_square}>KASHIM</div>
+  </div>
+);
 
 export default App;
