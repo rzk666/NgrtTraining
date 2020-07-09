df213e0affd77cf9310dd32cbdae0ccbc886793d
Razi <razi@journeyo.com>
2020-07-09 18:33:33 +0300

    [WIP] Nagorath Squares

--- Basic React/my_first_react_app/src/App.jsx
+++ Basic React/my_first_react_app/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+// Components
+import NagorathSquare from './components/Homepage/NagorathSquare';
 // styles
 import styles from './App.module.scss';
 
-const App = () => (
-  <div className={styles.site_container}>
-    <div className={styles.blue_square}>ZLADIM</div>
-    <div className={styles.green_square}>KASHIM</div>
-  </div>
-);
+const App = () => {
+  const x = Math.floor(Math.random() * (0 + 1, 100) + 5);
+  const y = Math.floor(Math.random() * (0 + 1, 100) + 5);
+  return (
+    <div className={styles.site_container}>
+      <NagorathSquare number={x} color="blue" />
+      <NagorathSquare number={y} color="zladim" />
+    </div>
+  );
+};
 
 export default App;
