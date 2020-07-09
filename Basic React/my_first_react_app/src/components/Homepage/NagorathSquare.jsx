ba159e1f0b4ac7ddef4a3fc595f9b611195b795d
Razi <razi@journeyo.com>
2020-07-09 18:45:11 +0300

    [WIP] onclick div

--- Basic React/my_first_react_app/src/components/Homepage/NagorathSquare.jsx
+++ Basic React/my_first_react_app/src/components/Homepage/NagorathSquare.jsx
@@ -4,14 +4,9 @@ import styles from './NagorathSquare.module.scss';
 
 const NagorathSquare = (props) => {
   const { color, number } = props;
-  // let color;
-  // let children;
-  // color = props['color']
-  // children = props.children
-  // console.log(children); console.log(color);
   let square;
   if (color === 'blue') {
-    square = <div className={styles.blue_square}>{number}</div>;
+    square = <div onClick={() => console.log('')} className={styles.blue_square}>{number}</div>;
   } else {
     square = <div className={styles.green_square}>{number}</div>;
   }
