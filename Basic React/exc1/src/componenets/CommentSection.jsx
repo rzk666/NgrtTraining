a116895e8ef1a213217327fb7450deb953b25547
yotam <ynagor@gmail.com>
2020-07-31 15:33:53 +0300

    {}

--- Basic React/exc1/src/componenets/CommentSection.jsx
+++ Basic React/exc1/src/componenets/CommentSection.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 // styles
+import { Input, Button } from 'semantic-ui-react';
 import styles from './CommentSection.module.scss';
+// componenets
 
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      comments: [],
       currentComment: '',
+      currentSearch: '',
     };
+  }
+
+  updateCurrentCommet(currentComment) {
+    this.setState({currentComment});
   }
 
   render() {
     return (
       <div className={styles.comments_container}>
-        5
+        <div className={styles.content_wrapper}>
+          <div className={styles.inputs_wrapper}>
+            <Input placeholder="Enter comment" className={styles.input_bar} fluid icon="search" onChange={(event) => console.log(event.currentTarget.value)}/>
+            <Button className={styles.post_btn} icon="chevron down" />
+            <Input placeholder="Search..." className={styles.search_bar} fluid icon="search" />
+
+          </div>
+          <div className={styles.comments}>
+            5
+          </div>
+        </div>
       </div>
     );
   }
--- Basic React/exc1/src/index.js
+++ Basic React/exc1/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './global.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
