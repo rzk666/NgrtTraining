d304e7c98bafb2ea2073c95ad516c26394f9fc70
Razi <razi@journeyo.com>
2020-08-09 19:24:22 +0300

    [IMPORTANT] Working active square exec -> How to work with array in react

--- Basic React/exc2/src/App.jsx
+++ Basic React/exc2/src/App.jsx
@@ -2,51 +2,79 @@ import React from 'react';
 import styles from './App.module.scss';
 import Square from './Componenets/Square.jsx'
 
+// ----- Consts & Dicts ----- //
+const COLORS_ARRAY = ['red', 'yellow', 'blue', 'green', 'grey'];
 
+// ----- Help Function ----- //
+const initSquares = (squares) => {
+  const squaresArray = [];
+  for (let index = 0; index < squares; index++) {
+    let randomColors = COLORS_ARRAY[Math.floor(Math.random()*COLORS_ARRAY.length)];
+    squaresArray.push(randomColors);
+  }
+  return (squaresArray);
+}
 
-function App() {
-  const colorsArray = ['red', 'yellow', 'blue', 'green', 'grey'];
-  let RandomColors = colorsArray[Math.floor(Math.random()*colorsArray.length)];
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeSquare: 0,
+    }
+    this.SQUARES = initSquares(10);
+  }
 
-  return (
-    <div className={styles.site_container}>
-      <div className={styles.squares_container}>
-        <Square color={RandomColors}/>
-        <Square color={RandomColors}/>
-        <Square color={RandomColors}/>
-        <Square color={RandomColors}/>
-        <Square color={RandomColors}/>
+  setActiveSquare(activeSquare) {
+    this.setState({ activeSquare }) 
+  }
+
+  render() {
+    const { activeSquare } = this.state;
+    return (
+      <div className={styles.site_container}>
+        {this.SQUARES.map((square, index) => (
+          <Square 
+            onClick={() => this.setActiveSquare(index)} 
+            color={square}
+            isActive={index === activeSquare}/>
+        ))}
+        {/* <div className={styles.squares_container}>
+          <Square color={RandomColors}/>
+          <Square color={RandomColors}/>
+          <Square color={RandomColors}/>
+          <Square color={RandomColors}/>
+          <Square color={RandomColors}/>
+        </div>
+        <div className={styles.squares_container}>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+        </div>
+        <div className={styles.squares_container}>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+        </div>
+        <div className={styles.squares_container}>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+        </div>
+        <div className={styles.squares_container}>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+          <Square/>
+        </div> */}
       </div>
-      <div className={styles.squares_container}>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-      </div>
-      <div className={styles.squares_container}>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-      </div>
-      <div className={styles.squares_container}>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-      </div>
-      <div className={styles.squares_container}>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-        <Square/>
-      </div>
-    </div>
-  );
+    )};
 }
 
 export default App;
